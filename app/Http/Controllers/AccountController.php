506f2556b6f8a88c8df230a249c1664b6625e739
venodon <venodon@gmail.com>
2021-12-03 20:41:18 +0300

    Избавляемся от "толстых" контроллеров + рефакторинг + небольшая оптимизация

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,18 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\LoyaltyAccount;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return mixed
+     */
     public function create(Request $request)
     {
         return LoyaltyAccount::create($request->all());
     }
 
+    /**
+     * @param $type
+     * @param $id
+     * @return JsonResponse
+     */
     public function activate($type, $id)
     {
-        if (($type == 'phone' || $type == 'card' || $type == 'email') && $id != '') {
+        if ($id && in_array($type,['phone', 'card', 'email'])) {
             if ($account = LoyaltyAccount::where($type, '=', $id)->first()) {
                 if (!$account->active) {
                     $account->active = true;
@@ -27,13 +37,17 @@ class AccountController extends Controller
         } else {
             throw new \InvalidArgumentException('Wrong parameters');
         }
-
         return response()->json(['success' => true]);
     }
 
+    /**
+     * @param $type
+     * @param $id
+     * @return JsonResponse
+     */
     public function deactivate($type, $id)
     {
-        if (($type == 'phone' || $type == 'card' || $type == 'email') && $id != '') {
+        if ($id && in_array($type,['phone', 'card', 'email'])) {
             if ($account = LoyaltyAccount::where($type, '=', $id)->first()) {
                 if ($account->active) {
                     $account->active = false;
@@ -46,21 +60,22 @@ class AccountController extends Controller
         } else {
             throw new \InvalidArgumentException('Wrong parameters');
         }
-
         return response()->json(['success' => true]);
     }
 
+    /**
+     * @param $type
+     * @param $id
+     * @return JsonResponse
+     */
     public function balance($type, $id)
     {
-        if (($type == 'phone' || $type == 'card' || $type == 'email') && $id != '') {
+        if ($id && in_array($type,['phone', 'card', 'email'])) {
             if ($account = LoyaltyAccount::where($type, '=', $id)->first()) {
                 return response()->json(['balance' => $account->getBalance()], 400);
-
-            } else {
-                return response()->json(['message' => 'Account is not found'], 400);
             }
-        } else {
-            throw new \InvalidArgumentException('Wrong parameters');
+            return response()->json(['message' => 'Account is not found'], 400);
         }
+        throw new \InvalidArgumentException('Wrong parameters');
     }
 }
